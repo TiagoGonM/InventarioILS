bced8a74eec83665d80a26b732f2fced659bca0d
Tiago <tiagogonzalez2024@gmail.com>
2025-11-10 10:32:44 -0300

    SeeOrder -> ViewOrderPopup

---
+++ InventarioILS/View/UserControls/ViewOrderPopup.xaml.cs
@@ -0,0 +1,27 @@
+﻿using InventarioILS.Model;
+using System;
+using System.Windows;
+
+namespace InventarioILS.View.UserControls
+{
+    /// <summary>
+    /// Lógica de interacción para ViewOrderPopup.xaml
+    /// </summary>
+    public partial class ViewOrderPopup : Window
+    {
+        public ViewOrderPopup()
+        {
+            InitializeComponent();
+
+            var orderItems = new OrderItems();
+            orderItems.LoadSingle(1);
+            DataContext = new
+            {
+                OrderName = "Pedido #1",
+                OrderDescription = "pedido de prueba",
+                OrderCreationDate = DateTime.Now.ToString("dd/MM/yyyy"),
+                orderItems.Items
+            };
+        }
+    }
+}
