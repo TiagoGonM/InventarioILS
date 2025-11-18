20b799a797d4483d17da5b2c566a5259076715d2
Tiago <tiagogonzalez2024@gmail.com>
2025-11-18 19:04:29 -0300

    replace enums with strings

--- InventarioILS/Model/Item.cs
+++ InventarioILS/Model/Item.cs
@@ -1,36 +1,15 @@
 ﻿namespace InventarioILS.Model
 {
-    public enum Class
-    {
-        INSUMO = 1,
-        REPUESTO = 2,
-        DISPOSITIVO = 3
-    }
-
-    public enum State
-    {
-        DEVICE_FUNCIONAL = 6,
-        DEVICE_REPARAR = 7,
-        DEVICE_DESCARTAR = 8,
-        
-        INSUMO_SUFICIENTE = 1,
-        INSUMO_ACOMPRAR = 2,
-        
-        REPUESTO_DISPONIBLE = 3,
-        REPUESTO_USADO = 4,
-        REPUESTO_DESCARTAR = 5 
-    }
-
     public abstract class Item : IIdentifiable
     {
         public int Id { get; set; }
         public string ProductCode { get; set; }
         public string CategoryName { get; set; }
         public string SubcategoryName { get; set; }
-        public Class Class { get; set; }
+        public string Class { get; set; }
         public string Description { get; set; }
         
-        protected Item(string productCode, string categoryName, string subcategoryName, string description, Class type)
+        protected Item(string productCode, string categoryName, string subcategoryName, string description, string type)
         {
             ProductCode = productCode;
             CategoryName = categoryName;
@@ -49,13 +28,14 @@
             string categoryName,
             string subcategoryName,
             string description,
-            Class type,
+            string type,
             string state,
             string location,
             int quantity,
             string additionalNotes = "") 
             : base(productCode, categoryName, subcategoryName, description, type)
         {
+            
             State = state;
             Location = location;
             Quantity = quantity;
