66dbf445749fd73e79356a4425476758a42081a5
Tiago <tiagogonzalez2024@gmail.com>
2025-11-18 19:07:13 -0300

    add event after confirming an add item form containing resulting item (WIP)

--- InventarioILS/View/UserControls/AddItem.xaml.cs
+++ InventarioILS/View/UserControls/AddItem.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using InventarioILS.Model;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace InventarioILS.View.UserControls
 {
@@ -20,9 +10,104 @@ namespace InventarioILS.View.UserControls
     /// </summary>
     public partial class AddItem : UserControl
     {
+        readonly ItemCategories categories = null;
+        readonly ItemSubCategories subCategories = null;
+        readonly ItemClasses classes = null;
+
+        string selectedCategory = "";
+        string selectedSubcategory = "";
+        string selectedClass = "";
+
         public AddItem()
         {
             InitializeComponent();
+
+            categories = ItemCategories.Instance;
+            subCategories = ItemSubCategories.Instance;
+            classes = ItemClasses.Instance;
+
+            DataContext = new
+            {
+                CategoryList = categories.Items,
+                SubcategoryList = subCategories.Items,
+                ClassList = classes.Items,
+            };
         }
+
+        // Evento personalizado OnConfirm
+        public event EventHandler<StockItemEventArgs> OnConfirm;
+
+        private void UpdateDescription()
+        {
+            DescriptionInput.Text = $"{selectedCategory} {selectedSubcategory}";
+        }
+
+        private void CategoryComboBox_SelectedItemChanged(object sender, System.EventArgs e)
+        {
+            var combo = (QueryableComboBox)sender;
+
+            var category = (Category)combo.SelectedItem;
+
+            if (category == null) return;
+
+            CodeCategoryShorthand.Text = category.Shorthand;
+
+            selectedCategory = category.Name;
+
+            UpdateDescription();
+        }
+
+        private void SubcategoryComboBox_SelectedItemChanged(object sender, System.EventArgs e)
+        {
+            var combo = (QueryableComboBox)sender;
+
+            var subcat = (ItemMisc)combo.SelectedItem;
+
+            if (subcat == null) return;
+
+            selectedSubcategory = subcat.Name;
+
+            UpdateDescription();
+        }
+
+        private void ClassComboBox_SelectedItemChanged(object sender, System.EventArgs e)
+        {
+            var combo = (QueryableComboBox)sender;
+
+            var itemClass = (ItemMisc)combo.SelectedItem;
+
+            if (itemClass == null) return;
+
+            selectedClass = itemClass.Name;
+        }
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedCategory) || string.IsNullOrEmpty(selectedSubcategory) || string.IsNullOrEmpty(selectedClass))
+                return;
+
+            // Crear la instancia de StockItem con los datos del formulario
+            var stockItem = new StockItem(
+                productCode: "",
+                categoryName: selectedCategory,
+                subcategoryName: selectedSubcategory,
+                description: DescriptionInput.Text,
+                type: selectedClass,
+                state: "",
+                location: "",
+                quantity: 0,
+                additionalNotes: NotesInput.Text
+            );
+            // Disparar el evento OnConfirm
+            OnConfirm?.Invoke(this, new StockItemEventArgs(stockItem));
+        }
+    }
+
+    /// <summary>
+    /// Clase personalizada para pasar datos del evento OnConfirm
+    /// </summary>
+    public class StockItemEventArgs(StockItem stockItem) : EventArgs
+    {
+        public StockItem Item { get; } = stockItem;
     }
 }
--- InventarioILS/View/UserControls/AddItemSection.xaml.cs
+++ InventarioILS/View/UserControls/AddItemSection.xaml.cs
@@ -1,9 +1,5 @@
 ﻿using InventarioILS.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace InventarioILS.View.UserControls
@@ -13,28 +9,14 @@ namespace InventarioILS.View.UserControls
     /// </summary>
     public partial class AddItemSection : UserControl
     {
-        readonly ItemCategories categories = null;
-        readonly ItemSubCategories subCategories = null;
-        readonly ItemClasses classes = null;
-
         public AddItemSection()
         {
             InitializeComponent();
+        }
 
-            categories = new ItemCategories();
-            subCategories = new ItemSubCategories();
-            classes = new ItemClasses();
-
-            categories.Load();
-            subCategories.Load();
-            classes.Load();
-
-            this.DataContext = new
-            {
-                CategoryList = categories.Items,
-                SubcategoryList = subCategories.Items,
-                ClassList = classes.Items
-            };
+        private void AddItem_OnConfirm(object sender, StockItemEventArgs e)
+        {
+            MessageBox.Show($"Event fired!, got {e.Item.Description}");
         }
     }
 }
