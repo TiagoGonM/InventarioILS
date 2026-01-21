752c18f84eab5b4714d1ed80bd5bf8cd2dfac050
Tiago <tiagogonzalez2024@gmail.com>
2026-01-20 22:50:49 -0300

    define wizard steps on an enum, add comment with all the steps described

--- InventarioILS/View/Windows/CSVImportWindow.xaml.cs
+++ InventarioILS/View/Windows/CSVImportWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Controls;
 using static InventarioILS.Services.DataImportService;
 
 namespace InventarioILS.View.Windows
@@ -14,6 +15,17 @@ namespace InventarioILS.View.Windows
     /// </summary>
     public partial class CSVImportWindow : Window
     {
+        // 0?. Posibilidad de mapear columnas pre importación para evitar errores?
+        // 1. Setear shorthands para categorías y subcategorías
+        // 2. Relación categoría-subcategoría | A lo mejor relación clase-estado
+        // 3. Previsualización final
+        public enum WizardSteps
+        {
+            STEP1,
+            STEP2,
+            STEP3,
+        }
+
         public IEnumerable<SerializableItem> Items { get; set; }
         public string Categories { get; set; }
         public string Subcategories { get; set; }
@@ -23,6 +35,27 @@ namespace InventarioILS.View.Windows
         public CSVImportWindow()
         {
             InitializeComponent();
+        }
+
+        private void SetWindow(WizardSteps step, DataResponse data)
+        {
+            UserControl content = null;
+
+            switch (step)
+            {
+                case WizardSteps.STEP1:
+                    content = new SetShorthands(data.CategoryRecords, data.SubcategoryRecords);
+                    break;
+
+                case WizardSteps.STEP2:
+                    // content = 
+                    break;
+            }
+
+            if (content != null)
+            {
+                WizardControl.Content = content;
+            }
         }
 
         public CSVImportWindow(DataResponse data) : this()
@@ -42,7 +75,23 @@ namespace InventarioILS.View.Windows
                 States
             };
 
-            WizardControl.Content = new SetShorthands(data.CategoryRecords, data.SubcategoryRecords);
+            SetWindow(WizardSteps.STEP1, data);
+
+        }
+
+        private void SubmitBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NextPageBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PreviousPageBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
